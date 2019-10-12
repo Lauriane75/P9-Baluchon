dee77cb2e0ba3192e648fc25f7a7ca1fd1f3e724
Lauriane <lauriane@quitoque.fr>
2019-10-12 19:26:39 +0200

    Add empty project

---
+++ BaluchonApp/BaluchonApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaluchonApp
+//
+//  Created by Lauriane Haydari on 12/10/2019.
+//  Copyright © 2019 Lauriane Haydari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
